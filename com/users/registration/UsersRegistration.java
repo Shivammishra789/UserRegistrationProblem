0c5298e1d1dba2bfcc33c21a4a44fb91bab76035
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-08 22:11:22 +0530

    ADDED: Validated sample emails

--- com/users/registration/UsersRegistration.java
+++ com/users/registration/UsersRegistration.java
@@ -1,9 +1,23 @@
 package com.users.registration;
 
+import java.util.Scanner;
+
 public class UsersRegistration {
 
 	public static void main(String[] args) {
 
-		UserInput.userFirstName();	
+		System.out.println("Enter 1 to validate and enter user details \nEnter 2 to validate email samples");
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		if(input == 1) {
+			UserInput.userFirstName();
+		}
+		else if(input == 2) {
+			SampleEmailValidation.testForValidEmails();
+			SampleEmailValidation.testForNonValidEmails();
+		}
+		else {
+			System.out.println("enter valid input");
+		}	
 	}	
 }
